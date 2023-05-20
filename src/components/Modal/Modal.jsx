26e43f338c19fb72d603856ce577e3957520328a
bakmaiev <thisyoungrhyme@gmail.com>
2023-05-21 00:53:39 +0300

    Refactor Modal with React Hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,40 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 import { StyledModalOverlay } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeydown);
-  }
+export const Modal = ({ largeImageURL, altText, onClose }) => {
+  useEffect(() => {
+    const handleKeydown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeydown);
-  }
+    window.addEventListener('keydown', handleKeydown);
 
-  handleKeydown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+    };
+  }, [onClose]);
 
-  handleBackdropClick = e => {
+  const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const { largeImageURL, altText } = this.props;
-    return createPortal(
-      <StyledModalOverlay onClick={this.handleBackdropClick}>
-        <div className="modal">
-          <img src={largeImageURL} alt={altText} />
-        </div>
-      </StyledModalOverlay>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <StyledModalOverlay onClick={handleBackdropClick}>
+      <div className="modal">
+        <img src={largeImageURL} alt={altText} />
+      </div>
+    </StyledModalOverlay>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
