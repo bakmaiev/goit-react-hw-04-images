4e7c11b15db8f9fa76fe1d60487f862290f2ebe8
bakmaiev <thisyoungrhyme@gmail.com>
2023-05-21 00:53:53 +0300

    Refactor Searchbar with React Hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,48 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { StyledSearchbar } from './Searchbar.styled';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { toast } from 'react-toastify';
-export class Searchbar extends Component {
-  state = {
-    imagesName: '',
-  };
+export const Searchbar = ({ onSubmit }) => {
+  const [imagesName, setImagesName] = useState('');
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (this.state.imagesName.trim() === '') {
+    if (imagesName.trim() === '') {
       toast.error('Enter something!');
       return;
     }
 
-    this.props.onSubmit(this.state.imagesName);
-    this.setState({ imagesName: '' });
+    onSubmit(imagesName);
+    setImagesName('');
   };
 
-  handleChangeName = e => {
-    this.setState({ imagesName: e.currentTarget.value });
+  const handleChangeName = e => {
+    setImagesName(e.currentTarget.value);
   };
 
-  render() {
-    return (
-      <StyledSearchbar>
-        <form className="form" onSubmit={this.handleSubmit}>
-          <button type="submit" className="button">
-            <span className="button-label">Search</span>
-            <AiOutlineSearch className="icon" />
-          </button>
+  return (
+    <StyledSearchbar>
+      <form className="form" onSubmit={handleSubmit}>
+        <button type="submit" className="button">
+          <span className="button-label">Search</span>
+          <AiOutlineSearch className="icon" />
+        </button>
 
-          <input
-            className="input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.imagesName}
-            onChange={this.handleChangeName}
-          />
-        </form>
-      </StyledSearchbar>
-    );
-  }
-}
+        <input
+          className="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={imagesName}
+          onChange={handleChangeName}
+        />
+      </form>
+    </StyledSearchbar>
+  );
+};
