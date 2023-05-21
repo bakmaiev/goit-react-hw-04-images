705fe80f4cbe61bbb3580c18ff88ae2fc4fd058e
bakmaiev <thisyoungrhyme@gmail.com>
2023-05-21 13:35:53 +0300

    Add new styles to Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,19 @@
 import { ThreeDots } from 'react-loader-spinner';
+import { StyledLoaderWrapper } from './Loader.styled';
 
 export const Loader = () => {
   return (
-    <ThreeDots
-      height="80"
-      width="80"
-      radius="9"
-      color="#4fa94d"
-      ariaLabel="three-dots-loading"
-      wrapperStyle={{}}
-      wrapperClassName=""
-      visible={true}
-    />
+    <StyledLoaderWrapper>
+      <ThreeDots
+        height="80"
+        width="80"
+        radius="9"
+        color="#3f51b5"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClassName
+        visible={true}
+      />
+    </StyledLoaderWrapper>
   );
 };
